0c46fe29d3e29cc9b0edfda013f419385e12dd60
amtb <alpha.mt.barry@gmail.com>
2020-05-09 18:47:55 +0400

    reduce

---
+++ src/performancejs/easy/6-reduce/reduce.spec.js
@@ -0,0 +1,30 @@
+import { reduce } from './reduce';
+
+describe('reduce', () => {
+  it('should return the correct value', () => {
+    expect(
+      reduce(
+        [1, 2, 3, 4], (a, b) => a + b, 0
+      )
+    ).toBe(10);
+
+    expect(
+      reduce(
+        ['foo', 'bar'], (acc, v) => {
+          acc[v] = v;
+          return acc;
+        }, {}
+      )
+    ).toEqual({
+      foo: 'foo', bar: 'bar'
+    });
+  });
+
+  it('should return the correct value when there is no initial value', () => {
+    expect(
+      reduce(
+        [1, 2, 3, 4], (a, b) => a + b
+      )
+    ).toBe(10);
+  });
+});
