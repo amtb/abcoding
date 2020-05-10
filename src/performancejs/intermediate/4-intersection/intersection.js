b95bc1ed95b52f19027303f5305791548ace1816
amtb <alpha.mt.barry@gmail.com>
2020-05-10 18:26:01 +0400

    Array intersection

---
+++ src/performancejs/intermediate/4-intersection/intersection.js
@@ -0,0 +1,15 @@
+/**
+ * Find the intersection of two arrays.
+ */
+export function intersection (array1, array2) {
+  const set2 = new Set(array2);
+
+  const found = [];
+  for (const i of array1) {
+    if (set2.has(i)) {
+      found.push(i);
+    }
+  }
+
+  return found;
+}
