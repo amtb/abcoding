ac321669244d8e93963dfaa05321072f8ffdf865
amtb <alpha.mt.barry@gmail.com>
2020-05-10 21:40:03 +0400

    leetcode Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ src/leetcode/1379-find-node-in-tree-clone/get-target-copy.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function (original, cloned, target) {
+
+  function find (node, cloneNode) {
+    if (!node) {
+      return;
+    }
+
+    if (node === target) {
+      return cloneNode;
+    }
+
+    if (node.left) {
+      const targetInClone = find(node.left, cloneNode.left);
+      if (targetInClone) {
+        return targetInClone;
+      }
+    }
+
+    if (node.right) {
+      return find(node.right, cloneNode.right);
+    }
+  }
+
+  return find(original, cloned);
+};
+
+export default getTargetCopy;
