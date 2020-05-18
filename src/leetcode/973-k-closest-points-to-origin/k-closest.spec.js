6a660a813ef8b0a0fc99073675ce975bbac29415
amtb <alpha.mt.barry@gmail.com>
2020-05-18 23:16:50 +0400

    k closest points to origin

---
+++ src/leetcode/973-k-closest-points-to-origin/k-closest.spec.js
@@ -0,0 +1,26 @@
+import kClosest from './k-closest';
+
+it('kClosest', () => {
+  expect(
+    kClosest(
+      [
+        [1, 3],
+        [-2, 2]
+      ],
+      1
+    )
+  ).toEqual([[-2, 2]]);
+  expect(
+    kClosest(
+      [
+        [3, 3],
+        [5, -1],
+        [-2, 4]
+      ],
+      2
+    )
+  ).toEqual([
+    [3, 3],
+    [-2, 4]
+  ]);
+});
