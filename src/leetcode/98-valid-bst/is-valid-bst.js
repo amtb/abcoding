9158899666b0e9db8addfd5f8f488c043f289c47
amtb <alpha.mt.barry@gmail.com>
2020-05-18 16:28:33 +0400

    lc 94 inorder traversal

--- src/leetcode/98-valid-bst/is-valid-bst.js
+++ src/leetcode/98-valid-bst/is-valid-bst.js
@@ -1,14 +1,4 @@
 /**
- * Definition for a binary tree node.
- */
-
-export function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
-}
-
-/**
  * @param {TreeNode} root
  * @return {boolean}
  */
