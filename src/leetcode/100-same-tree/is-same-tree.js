eaa5c92c9dda7dfefbe9ee433c9f066e18f09a90
amtb <alpha.mt.barry@gmail.com>
2020-05-19 23:19:46 +0400

    lc 100 same tree

---
+++ src/leetcode/100-same-tree/is-same-tree.js
@@ -0,0 +1,17 @@
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (p && q) {
+    return (
+      p.val === q.val &&
+      isSameTree(p.left, q.left) &&
+      isSameTree(p.right, q.right)
+    );
+  }
+  return p === q;
+};
+
+export default isSameTree;
