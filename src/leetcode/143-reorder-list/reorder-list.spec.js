ac1903bc9666acb7f8868e33d561c6eb6cca6d2b
amtb <alpha.mt.barry@gmail.com>
2020-06-09 12:54:24 +0400

    fix build

--- src/leetcode/143-reorder-list/reorder-list.spec.js
+++ src/leetcode/143-reorder-list/reorder-list.spec.js
@@ -1,12 +1,12 @@
-import { arrayToList } from '../../ds';
+import { LinkedList } from '../../ds';
 import reorderList from './reorder-list';
 
 it('reorderList', () => {
-  const list1 = arrayToList([1, 2, 3, 4]);
+  const list1 = LinkedList.fromArray([1, 2, 3, 4]);
   reorderList(list1);
-  expect(list1).toEqual(arrayToList([1, 4, 2, 3]));
+  expect(list1).toEqual(LinkedList.fromArray([1, 4, 2, 3]));
 
-  const list2 = arrayToList([1, 2, 3, 4, 5]);
+  const list2 = LinkedList.fromArray([1, 2, 3, 4, 5]);
   reorderList(list2);
-  expect(list2).toEqual(arrayToList([1, 5, 2, 4, 3]));
+  expect(list2).toEqual(LinkedList.fromArray([1, 5, 2, 4, 3]));
 });
