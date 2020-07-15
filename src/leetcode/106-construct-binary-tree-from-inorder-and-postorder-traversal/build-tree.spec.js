0f26e4af5a5b72f05f455272726ef4782aac7948
amtb <alpha.mt.barry@gmail.com>
2020-07-15 22:57:30 +0400

    leetcode 189 rotate array

--- src/leetcode/106-construct-binary-tree-from-inorder-and-postorder-traversal/build-tree.spec.js
+++ src/leetcode/106-construct-binary-tree-from-inorder-and-postorder-traversal/build-tree.spec.js
@@ -7,10 +7,6 @@ it('buildTree', () => {
     new TreeNode(9),
     new TreeNode(20, new TreeNode(15), new TreeNode(7))
   );
-
-  // console.log(tree);
-
   const built = buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]);
-  console.log(built);
   expect(built).toEqual(tree);
 });
