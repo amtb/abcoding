9288493adc25058e084374f6bb6de6b12d50b5cf
amtb <alpha.mt.barry@gmail.com>
2020-07-29 14:42:17 +0400

    leetcode 118 pascal's triangle

---
+++ src/leetcode/118-pascals-triangle/generate.spec.js
@@ -0,0 +1,11 @@
+import generate from './generate';
+
+it('generate', () => {
+  expect(generate(5)).toEqual([
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1]
+  ]);
+});
